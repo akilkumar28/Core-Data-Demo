133670f8adb4af921782bddce414d86e1af37231
Akil Kumar Thota <akthota@microsoft.com>
2019-12-17 12:48:41 +0530

    Intial commit

--- Core Data Demo/HomeVC.swift
+++ Core Data Demo/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Core Data Demo
 //
 //  Created by AKIL KUMAR THOTA on 12/17/19.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
